9a162aa93ffd7d7983697a7019bf14b0d45cd0e6
Abhijit Balaji <abhijit@Abhijits-MacBook-Pro.local>
2025-06-14 14:35:45 -0700

    remove meta device placement in MXInferenceLinear

--- torchmx/layers/mx_linear.py
+++ torchmx/layers/mx_linear.py
@@ -42,19 +42,14 @@ class MXInferenceLinear(torch.nn.Linear):
             }
             new_mod = cls(**super_kwargs)
         new_mod.qconfig = qconfig
-        if not mod.weight.data.device == torch.device("meta"):
-            # Do NOT quantize on `meta` device as it causes issues with `accelerate`
-            # This line fails: https://github.com/huggingface/accelerate/blob/b431d1f02733dfcb1668c7f3cffe1e2f25fb94e7/src/accelerate/utils/modeling.py#L366
-            # They manually patch for `torchao`: https://github.com/huggingface/accelerate/blob/b431d1f02733dfcb1668c7f3cffe1e2f25fb94e7/src/accelerate/utils/modeling.py#L353
-            # The simpler solution for now is to not quantize here but to quantize everytime during the forward pass when the weights are on the meta device.
-            new_mod.weight = torch.nn.Parameter(
-                MXTensor.to_mx(
-                    mod.weight.data,
-                    qconfig.weights_config.elem_dtype,
-                    qconfig.weights_config.block_size,
-                ),
-                requires_grad=False,
-            )
+        new_mod.weight = torch.nn.Parameter(
+            MXTensor.to_mx(
+                mod.weight.data,
+                qconfig.weights_config.elem_dtype,
+                qconfig.weights_config.block_size,
+            ),
+            requires_grad=False,
+        )
         new_mod.bias = mod.bias
         return new_mod
 
@@ -65,31 +60,5 @@ class MXInferenceLinear(torch.nn.Linear):
             self.qconfig.activations_config.elem_dtype,
             self.qconfig.activations_config.block_size,
         )
-        if not isinstance(self.weight.data, MXTensor):
-            # This case happens when we are performing big model inference using
-            # `accelerate` and the weights are on the disk, `accelerate` puts the
-            # weights on the `meta` device. For more information, refer to the comment
-            # in the from_float() above.
-
-            # Do not modify the self.weight and directly send the quantized weights to
-            # F.linear() so that casting back to `meta` device works correctly inside
-            # `accelerate`.
-
-            # For some reason the weights in the disk when loaded are in `torch.float32`
-            # and do `NOT` respect the dtype passed when creating the HF model. So we
-            # have to cast them to the correct dtype. We only support MX quant for
-            # torch.bfloat16.
-
-            # Cast bias to torch.bfloat16 for safety
-            return F.linear(
-                x_mx,
-                MXTensor.to_mx(
-                    self.weight.data.to(torch.bfloat16),
-                    self.qconfig.weights_config.elem_dtype,
-                    self.qconfig.weights_config.block_size,
-                ),
-                self.bias.to(torch.bfloat16) if self.bias else self.bias,
-            )
-        else:
-            y = F.linear(x_mx, self.weight, self.bias)
+        y = F.linear(x_mx, self.weight, self.bias)
         return y
